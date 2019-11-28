2ee65e4012f754bb8217ffc47b1c67cee908cc48
takagotch <dyaccb@gmail.com>
2019-11-29 07:31:14 +0900

    __fetch_ohlcv_list(symbol=BTCUSD, period=1m, start=0, end=0):

--- fr1/list_df_timestamp_csv.py
+++ fr1/list_df_timestamp_csv.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 from datetime import datetime, timedelta
 import time, calendar, pytz, requests
 import pandas as pd
@@ -20,19 +21,100 @@ def main():
   1st_ohlcv = fetch_ohlcv_1st(period="1m", count=10, reverse=False, partial=)
   print(1st_ohlcv)
 
-  def fetch_ohlcv_1st(period="1m", symbol="XBTUSD", count=1000, reverse=T)
+  def fetch_ohlcv_lst(period="1m", symbol="XBTUSD", count=1000, reverse=T)
+    df = fetch_ohlcv_df(period, symbol, count, reverse, partial, tstype)
+    if df is None:
+      return None
+    return [[int(x[0]) if tstype=="UTS" or tstype=="UTMS" else x[0], x[1], x[2], x[3], x[4], int(x[5])] for x in df.values.tolist()]
 
-  def fetch_ohlcv_df()
+  def fetch_ohlcv_df(period="1m", symbol="XBTUSD", count=1000, reverse=True, partial=False, tstype="UTMS"):
+    if peroid not in ALLOWED_PERIOD:
+      return None
+    period_params = ALLOWED_PERIOD[period]
+      return None
+    need_count = (count + 1)* period_params[1]  
+    
+    df_ohlcv = __get_ohlcv_paged(symbol=symbol, period=period_paams[0], count=need_count)
+
+    if period_params[1] > 1:
+      minutes = ALLOWED+PEROID[period][2]
+      offset = str(minutes) + "T"
+      if 60 <= minutes < 1440:
+        offset = str(minutes / 60) + "H"
+      elif 1440 <= minutes:
+        offset = str(minutes / 1440) + "D"
+      df_ohlcv = df_ohlcv.resample(offset).agg({
+          "timestamp": "first",
+          "open": "first",
+          "high": "max",
+          "low": "min",
+          "close": "last",
+          "volume": "sum",
+        })
+      
+      if partial == False:
+        df_ohlcv = df_ohlcv.iloc[:-1]
+
+      if len(df_ohlcv) > count:
+        df_ohlcv = df_ohlcv.iloc[len(df_ohlcv)-count:]
+
+      df_ohlcv.reset_index(inplace=True)
+
+      df_ohlcv["datetime"] += timedelta(minutes=ALLOWED_PERIOD[period][2])
+
+      __convert_timestamp(df_ohlcv, tstype)
+
+      df_ohlcv.drop("datetime", axis=1, inplace=True)
+
+      if reverse == True:
+        df_ohlcv = df_ohlcv.iloc[::-1]
+      
+      df_ohlcv.reset_index(inplace=True, drop=True)
+      return df_ohlcv
 
   #private
-  def __convert_timestamp():
-    if timestamp == "":
-    elif timestamp == "":
-    elif
+  def __convert_timestamp(df_ohlcv, timestamp="UTMS"):
+    if timestamp == "UTS":
+      df_ohlcv["timestamp"] = pd.Series([int(dt.timestamp()) for dt in df_ohlcv["datetime"]])
+    elif timestamp == "UTMS":
+      df_ohlcv["timestamp"] = pd.Series([int(dt.timestamp()) * 1000 for dt in df_ohlcv["datetime"]])
+    elif timestamp == "DT":
+      df_ohlcv["timestamp"] = df_ohlcv["datetime"]
+    elif timestamp == "STS":
+      df_ohlcv["timestamp"] = pd.Series([dt.strftime("%Y-%m-%d%T-%H:%M:%S")for dt in df_ohlcv["datetime"]])
+    elif timestamp == "STMS":
+      df_ohlvc["timestamp"] = pd.Series([dt.strftime("%Y-%m-%d%T-%H:%M:%S.%fZ")for dt in df_ohlcv["datetime"]])
+    else:
+      df_ohlcv["timestamp"] =df_ohlcv["datetime"]
 
   def __get_ohlcv_paged(symbol="XBTUSD", period="1m", count=1000):
+    ohlcv_list = []
+    utc_now = datetime.now(pytz.utc)
+    to_time = int(utc_now.timestamp())
+    #to time = int(time.mktime(utc_now.timetuple()))
+    from_time = to_time - ALLOWED_PERIOD[period][2] * 60 * count
+    start = from_time
+    end = to_time
+    if count > 10000:
+      end = from_time + ALLOWED_PERIOD[period][2] * 60 * 10000
+    while start <= to_time:
+      ohlcv_list += __fetch_ohlcv_list(symbol=symbol, period=period, )
+      start = end + ALLOWED_PERIOD[period][2] * 60
+      end = start + ALLOWED_PERIOD[period][2] * 60 * 10000
+      if end > to_time:
+        end = to_time
+    df_ohlcv = pd.DateFrame(ohlcv_list,
+        columns=["timestamp", "open", "high", "low", "close", "volume"])
+    df_ohlcv["datetime"] = pd.to_datetime("datetime")
+    df_ohlcv.index = df_ohlcv.index.tz_localize("UTC")
+    return df_ohlcv
 
   def __fetch_ohlcv_list(symbol="XBTUSD", period="1m", start=0, end=0):
+    param = {"period":ALLOWED_PERIOD[period][2], "from":start, "to":end}
+    url = "https://www.bitmex.com/api/udf/history?symbol=XBTUSD&resolution={period}&from={from}&to={to}".format(**param)
+    res = requsts.get(url)
+    data = res.json()
+    return[list(ohlcv) for ohlcv in zip(data["t"],data["o"],data["h"],data["l"],data["c"],data["v"])]
 
   if __name__ == "__main__":
     main()
